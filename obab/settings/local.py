9b842a93bf9a2532a3155afead99c0ba19022212
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-11-17 09:32:54 +0900

    postgresql 연동

--- obab/settings/local.py
+++ obab/settings/local.py
@@ -1,3 +1,14 @@
 from .base import *
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+    }
+}
--- obab/settings/prod.py
+++ obab/settings/prod.py
@@ -1,3 +1,14 @@
 from .base import *
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
