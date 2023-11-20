fe1f16ae668f3bd525756a05a6b2564aa6f9bd3c
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-11-20 16:00:47 +0900

    black 패키지 설치 및 실행

--- obab/settings/prod.py
+++ obab/settings/prod.py
@@ -1,15 +1,15 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
