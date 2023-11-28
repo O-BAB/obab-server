b973805fa1ab197de1267d791fd37ce8c14d2153
백종성 <jongseoung@192.168.nate.com>
2023-11-28 10:19:46 +0900

    GPT를 이용해 메뉴 추천 받는 기능 추가
    gpt 계정이 유료 계정이 아니여서 현재 사용 불가능
    추후에 유료계정 전환 후 사용

--- foods/functions.py
+++ foods/functions.py
@@ -1,16 +1,18 @@
-from googletrans import Translator
-
-
-def translate_to_english(korean_text):
-    translator = Translator()
-    translation = translator.translate(korean_text, src='ko', dest='en')
-    english_text = translation.text
-    return english_text
-
-
-def translate_to_korea(english_text):
-    translator = Translator()
-    translation = translator.translate(english_text, src='en', dest='ko')
-    korean_text = translation.text
-    return korean_text
-
+# from openai import OpenAI
+# from django.conf import settings
+#
+#
+# def ask_openai(message):
+#     client = OpenAI(
+#         api_key=getattr(settings, "OPENAI_API_KEY")
+#     )
+#
+#     message = str(message) + "한국어로 답변해줘"
+#     completion = client.chat.completions.create(
+#       model="gpt-3.5-turbo",
+#       messages=[
+#         {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+#         {"role": "user", "content": message}
+#       ]
+#     )
+#     return completion.choices[0].message
