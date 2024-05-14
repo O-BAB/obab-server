03dc21876a09475e2ae079663bfb6c396a2f83d7
beak jong seoung <jjong015189@gmail.com>
2024-05-14 21:56:12 +0900

    refactor: cors #19
    - base_settings와 local_setting에 중복되어 있던 cors관련 설정 제거

--- obab_server/settings/base_settings.py
+++ obab_server/settings/base_settings.py
@@ -58,9 +58,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ORIGIN_WHITELIST = ("http://127.0.0.1:3000", "http://localhost:3000")
-CORS_ALLOW_CREDENTIALS = True
-
 ROOT_URLCONF = "obab_server.urls"
 
 TEMPLATES = [
