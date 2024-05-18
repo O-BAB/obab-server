77ac5c6abf011395b611765749966fb9c6557bca
beak jong seoung <jjong015189@gmail.com>
2024-05-18 17:29:18 +0900

    chore: pre-commit

--- obab_server/settings/base_settings.py
+++ obab_server/settings/base_settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
-
 from datetime import timedelta
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
